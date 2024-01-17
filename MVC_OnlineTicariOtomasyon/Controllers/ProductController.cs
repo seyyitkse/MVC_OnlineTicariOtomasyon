aab5e1d8ac6af83dc107e175d5bc00e6b37ed20a
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-01-17 11:33:51 +0300

    Editing the product listing page and performing the listing process

---
+++ MVC_OnlineTicariOtomasyon/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using MVC_OnlineTicariOtomasyon.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_OnlineTicariOtomasyon.Controllers
+{
+    public class ProductController : Controller
+    {
+        Context DbProduct=new Context();
+        public ActionResult Index()
+        {
+            var product=(DbProduct.Products.Where(x=>x.ProductStatus==true)).ToList();
+            return View(product);
+        }
+    }
+}
