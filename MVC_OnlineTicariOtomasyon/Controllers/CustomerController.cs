49c52e5ce30d08f571bf251f6c13be39d8f4c126
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-01-18 21:46:54 +0300

    Customer listing completed.

---
+++ MVC_OnlineTicariOtomasyon/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using MVC_OnlineTicariOtomasyon.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_OnlineTicariOtomasyon.Controllers
+{
+    public class CustomerController : Controller
+    {
+        Context DbCustomer=new Context();
+        public ActionResult Index()
+        {
+            var customers = (DbCustomer.Customers.Where(x => x.CustomerStatus == true)).ToList();
+            return View(customers);
+        }
+    }
+}
