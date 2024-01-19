72add23321565364e34d984dd200981274182c8e
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-01-19 10:10:22 +0300

    Employee listing completed.

--- MVC_OnlineTicariOtomasyon/Controllers/EmployeeController.cs
+++ MVC_OnlineTicariOtomasyon/Controllers/EmployeeController.cs
@@ -9,6 +9,11 @@ namespace MVC_OnlineTicariOtomasyon.Controllers
 {
     public class EmployeeController : Controller
     {
-
+        Context DbEmployee = new Context(); 
+        public ActionResult Index() 
+        { 
+            var employee = (DbEmployee.Employees.Where(x => x.EmployeeStatus == true)).ToList(); 
+            return View(employee); 
+        }
     }
 }
