d39f8e8094dec0f59620ab4509fbd6b5748acf19
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-01-19 09:50:08 +0300

    Added empty, employee controller.

---
+++ MVC_OnlineTicariOtomasyon/Controllers/EmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using MVC_OnlineTicariOtomasyon.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_OnlineTicariOtomasyon.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+    }
+}
