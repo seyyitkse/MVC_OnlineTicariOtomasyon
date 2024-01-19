718590567f59ba51744fcc4659433bb873d2abf1
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-01-19 21:11:36 +0300

    Listing of sales has been enabled.

---
+++ MVC_OnlineTicariOtomasyon/Controllers/SalesController.cs
@@ -0,0 +1,19 @@
+﻿using MVC_OnlineTicariOtomasyon.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_OnlineTicariOtomasyon.Controllers
+{
+    public class SalesController : Controller
+    {
+        Context DbSales=new Context();
+        public ActionResult Index()
+        {
+            var sales=DbSales.Sales.ToList();
+            return View(sales);
+        }
+    }
+}
