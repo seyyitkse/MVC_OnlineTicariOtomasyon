5d639620b0f82a7d67b9baa56c74a72357b68a61
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-01-20 19:42:35 +0300

    The sale process was carried out.(Stock drop will be made after)

--- MVC_OnlineTicariOtomasyon/Controllers/SalesController.cs
+++ MVC_OnlineTicariOtomasyon/Controllers/SalesController.cs
@@ -9,11 +9,66 @@ namespace MVC_OnlineTicariOtomasyon.Controllers
 {
     public class SalesController : Controller
     {
-        Context DbSales=new Context();
+        Context DbSales = new Context();
+
+        public List<SelectListItem> employees
+        {
+            get
+            {
+                return (from item in DbSales.Sales.ToList()
+                        select new SelectListItem
+                        {
+                            Text = item.Employee.EmployeeName + " " + item.Employee.EmployeeSurname,
+                            Value = item.EmployeeID.ToString()
+                        }).ToList();
+            }
+        }
+        public List<SelectListItem> customers
+        {
+            get
+            {
+                return (from item in DbSales.Customers.ToList()
+                        select new SelectListItem
+                        {
+                            Text = item.CustomerName + " " + item.CustomerSurname,
+                            Value = item.CustomerID.ToString()
+                        }).ToList();
+            }
+        }
+
+        public List<SelectListItem> products
+        {
+            get
+            {
+                return (from item in DbSales.Products.ToList()
+                        select new SelectListItem
+                        {
+                            Text = item.ProductName +"("+item.ProductBrand+")",
+                            Value = item.ProductID.ToString()
+                        }).ToList();
+            }
+        }
         public ActionResult Index()
         {
-            var sales=DbSales.Sales.ToList();
+            var sales = DbSales.Sales.ToList();
             return View(sales);
+        }
+        [HttpGet]
+        public ActionResult NewSale()
+        {
+            ViewBag.Employees = employees;
+            ViewBag.Customers=customers;
+            ViewBag.Products=products;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewSale(Sales sales)
+        {
+            DbSales.Sales.Add(sales);
+            sales.SalesTotalPrice = sales.SalesPrice * sales.SalesQuantity;
+            sales.SalesDate= DateTime.Now;
+            DbSales.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
