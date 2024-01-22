a4ec5e8d719fafea0784de2d780b7eb6d2217638
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-01-22 09:50:40 +0300

    Added empty invoice controller.

---
+++ MVC_OnlineTicariOtomasyon/Controllers/InvoiceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_OnlineTicariOtomasyon.Models.Classes;
+
+namespace MVC_OnlineTicariOtomasyon.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        public ActionResult Index() 
+        {
+            return View();
+        }
+    }
+}
