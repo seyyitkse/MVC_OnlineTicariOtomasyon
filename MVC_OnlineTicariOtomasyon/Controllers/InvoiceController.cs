6cd2d9ed73ff97180bb9794bccbde089ee50f8e6
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-01-22 12:20:14 +0300

    Invoice adding process was done.(Non-dynamically)

--- MVC_OnlineTicariOtomasyon/Controllers/InvoiceController.cs
+++ MVC_OnlineTicariOtomasyon/Controllers/InvoiceController.cs
@@ -10,10 +10,53 @@ namespace MVC_OnlineTicariOtomasyon.Controllers
     public class InvoiceController : Controller
     {
         Context DbInvoice=new Context();
+        public List<SelectListItem> employees
+        {
+            get
+            {
+                return (from item in DbInvoice.Sales.ToList()
+                        select new SelectListItem
+                        {
+                            Text = item.Employee.EmployeeName + " " + item.Employee.EmployeeSurname,
+                            Value = item.EmployeeID.ToString()
+                        }).ToList();
+            }
+        }
+        public List<SelectListItem> customers
+        {
+            get
+            {
+                return (from item in DbInvoice.Customers.ToList()
+                        select new SelectListItem
+                        {
+                            Text = item.CustomerName + " " + item.CustomerSurname,
+                            Value = item.CustomerID.ToString()
+                        }).ToList();
+            }
+        }
+
         public ActionResult Index()
         {
             var invoices=(DbInvoice.Invoinces.Where(x=>x.InvoinceStatus==true)).ToList();
             return View(invoices);
+        }
+        [HttpGet]
+        public ActionResult AddInvoice()
+        {
+            ViewBag.Employees = employees;
+            ViewBag.Customers = customers;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddInvoice(Invoice invoice)
+        {
+            DbInvoice.Invoinces.Add(invoice);
+            invoice.InvoinceStatus = true;
+            invoice.InvoiceSerial = "A";
+            invoice.InvoinceDate= DateTime.Now;
+            invoice.InvoiceClock = DateTime.Now;
+            DbInvoice.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
