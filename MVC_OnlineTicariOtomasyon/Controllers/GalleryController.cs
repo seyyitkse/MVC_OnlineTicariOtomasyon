f61b1be7ca4b465ead91435b03e0e05f9544f5c7
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-01-23 17:15:16 +0300

    Added empty gallery controller and empty Gallery View

---
+++ MVC_OnlineTicariOtomasyon/Controllers/GalleryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_OnlineTicariOtomasyon.Models.Classes;
+
+namespace MVC_OnlineTicariOtomasyon.Controllers
+{
+    public class GalleryController : Controller
+    {
+        Context DbGallery = new Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
